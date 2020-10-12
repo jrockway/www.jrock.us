3786326ed290a5b6da654be7056b1f987077b9c8
Jonathan Rockway <2367+jrockway@users.noreply.github.com>
2020-10-12 03:10:11 -0400

    remove webpack's involvement with this project; we have no javascript to pack

--- postcss.config.js
+++ postcss.config.js
@@ -1,28 +1,38 @@
-const url = require("postcss-url");
 const imports = require("postcss-import");
+const mixins = require("postcss-mixins");
 const nested = require("postcss-nested");
 const postCSSPresetEnv = require("postcss-preset-env");
-const browsers = require("browserslist");
-const cssnano = require("cssnano");
 const color = require("postcss-color-mod-function");
-const mixins = require("postcss-mixins");
+const cssnano = require("cssnano");
+const url = require("postcss-url");
 
 module.exports = () => ({
-  plugins: [
-    url,
-    imports,
-    mixins,
-    nested,
-    postCSSPresetEnv({
-      stage: 1,
-      preserve: true,
-      features: {
-        "custom-properties": true,
-      },
-    }),
-    cssnano({
-      preset: "default",
-    }),
-    color,
-  ],
+    plugins: [
+        imports,
+        mixins,
+        nested,
+        postCSSPresetEnv({
+            stage: 1,
+            preserve: true,
+            features: {
+                "custom-properties": true,
+            },
+        }),
+        color,
+        url({
+            url: "copy",
+            // This kind of tries to preserve the original filename.  Necessary because it won't copy
+            // files when useHash is not true.
+            useHash: true,
+            hashOptions: {
+                method: (_) => {
+                    return "";
+                },
+                append: true,
+            },
+        }),
+        cssnano({
+            preset: "default",
+        }),
+    ],
 });
